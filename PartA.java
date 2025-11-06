cb7f592ca0c7691d45e437cd3897ef7d8d405220
jakeski2020 <26joliviero@derryfield.org>
2025-11-06 09:18:35 -0500

    Refactor input handling in PartA.java

--- PartA.java
+++ PartA.java
@@ -2,25 +2,19 @@ import java.util.Scanner;
 public class PartA {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         System.out.println("Enter test scores to calculate the average.");
         System.out.println("Enter a negative number to finish input.");
-        
         double sum = 0;
         int count = 0;
-        
         while (true) {
             System.out.print("Enter test score: ");
-            double score = scanner.nextDouble();
-            
+            double score = scanner.nextDouble();     
             if (score < 0) {
                 break;
-            }
-            
+            } 
             sum += score;
             count++;
         }
-        
         if (count > 0) {
             double average = sum / count;
             System.out.println("Number of scores entered: " + count);
@@ -28,7 +22,6 @@ public class PartA {
         } else {
             System.out.println("No scores were entered.");
         }
-        
         scanner.close();
     }
 }
